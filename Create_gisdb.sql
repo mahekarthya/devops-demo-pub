85cd6ba775d6b08848703b859f24d6762bff2841
mahekarthya-local <mahekarthya-local@gmail.com>
2026-04-26 21:06:35 +0530

    updated the file

--- Create_gisdb.sql
+++ Create_gisdb.sql
@@ -1,29 +1,34 @@
-
 \conninfo
 
+\echo ==== List Databases before change ====
 select datname from pg_database;
 
+\echo ==== Dropping Database gisdb if exist ====
 DROP DATABASE IF EXISTS gisdb;
 
+\echo ==== Creating Database gisdb ====
 CREATE DATABASE gisdb;
 
--- switch connection
+\echo ==== Switching to gisdb ====
 \c gisdb
 
 \conninfo
 
+\echo ==== Creating customer table in gisdb ====
 CREATE TABLE customer (
   id INT,
   custname VARCHAR(100)
 );
 
+\echo ==== Inserting rows into customer  ====
 INSERT INTO customer (id, custname) VALUES
 (1, 'ABC Co'),
 (2, 'Honda Co'),
-(3, 'Yamaha');
+(3, 'Yamaha'),
 (4, 'Hyundai');
 
-
+\echo ==== Display customer data ====
 Select * from customer;
 
+\echo ==== Final database list after change ====
 select datname from pg_database;
